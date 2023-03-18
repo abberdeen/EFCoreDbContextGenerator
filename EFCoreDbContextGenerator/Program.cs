326c8d1bc73fc8f772b1dfb2c570a1ae225a0387
abberdeen <abdurahim.khudoyberdiev@gmail.com>
2023-03-19 04:43:28 +0500

    Add project files.

---
+++ EFCoreDbContextGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿// Generate DBContext class automatically from Models folder
+// 
+
+string modelsFolder = null;
+foreach (string arg in args)
+{
+    if (arg.StartsWith("-f="))
+    {
+        modelsFolder = arg.Substring(3);
+    }
+}
+
+if (modelsFolder != null)
+{
+    Console.WriteLine($"{modelsFolder}!");
+}
+
+Console.WriteLine("");
